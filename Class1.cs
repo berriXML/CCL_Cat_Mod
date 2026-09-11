338473c54aec7cc449bd413359461e1387216690
icedm0cha <arein1787@gmail.com>
2026-09-11 09:46:33 -0400

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿public class CatGuardMod : IModApi
+{
+    public void InitMod(Mod _modInstance)
+    {
+        Log.Out("[CatGuard] Mod initialized!");
+
+        ModEvents.GameStartDone.RegisterHandler(OnGameStartDone);
+        ModEvents.PlayerSpawnedInWorld.RegisterHandler(OnPlayerSpawned);
+    }
+
+    private static void OnGameStartDone(
+        ref ModEvents.SGameStartDoneData _data)
+    {
+        Log.Out("[CatGuard] GAME START DONE!");
+    }
+
+    private static void OnPlayerSpawned(
+        ref ModEvents.SPlayerSpawnedInWorldData _data)
+    {
+        Log.Out("[CatGuard] PLAYER SPAWN EVENT FIRED!");
+    }
+}
